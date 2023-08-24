f397ece750807bbab6637eba0792c9b6a208e9a5
irmrz <ignaciotramirez02@gmail.com>
2023-08-24 20:22:33 -0300

    tabla continent creada

--- practico2/practico2.sql
+++ practico2/practico2.sql
@@ -1,15 +1,47 @@
 -- mysql -u root -h localhost
 
-CREATE DATABASE world;
+CREATE DATABASE IF NOT EXISTS `world`;
 
 USE `world`;
 
 CREATE TABLE city (
-    ID int NOT NULL PRIMARY KEY,
+    ID int NOT NULL PRIMARY KEY NOT NULL,
     Name varchar(35) NOT NULL,
-    CountyCode varchar(35) NOT NULL,
-    Disctrict varchar(35) NOT NULL,
-    Population int NOT NULL,
+    CountyCode varchar(35) FOREIGN KEY REFERENCES Country(Code),
+    Disctrict varchar(35),
+    Population int,
+)
+
+CREATE TABLE countrylanguage (
+    CountryCode varchar(35) FOREIGN KEY REFERENCES Country(Code) NOT NULL,
+    Language varchar(35) PRIMARY KEY NOT NULL,
+    IsOfficial varchar(35),
+    Percentage float,
+)
+
+CREATE TABLE country (
+    Code varchar(35) PRIMARY KEY NOT NULL,
+    Name varchar(35) FOREIGN KEY REFERENCES Continent(Name) NOT NULL,
+    Continent varchar(35),
+    Region varchar(35),
+    SurfaceArea float,
+    IndepYear int,
+    Population int,
+    LifeExpectancy float,
+    GNP float,
+    GNPOld float,
+    LocalName varchar(35),
+    GovernmentForm varchar(35),
+    HeadOfState varchar(35),
+    Capital int,
+    Code2 varchar(35),
+)
+
+CREATE TABLE continent (
+    Name varchar(35) PRIMARY KEY NOT NULL,
+    Area float,
+    MassPercentage float,
+    MostPopulatedCity varchar(35),
 )
 
 SOURCE world-data.sql;

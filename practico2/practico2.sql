27c504fa3bf0b0c9f53c111134721cff332bfcfc
irmrz <ignaciotramirez02@gmail.com>
2023-08-24 17:38:03 -0300

    actualizando

--- practico2/practico2.sql
+++ practico2/practico2.sql
@@ -1,7 +1,17 @@
-mysql -u root -h localhost
+-- mysql -u root -h localhost
 
 CREATE DATABASE world;
 
-USE world;
+USE `world`;
 
-CREATE TABLE continent
+CREATE TABLE city (
+    ID int NOT NULL PRIMARY KEY,
+    Name varchar(35) NOT NULL,
+    CountyCode varchar(35) NOT NULL,
+    Disctrict varchar(35) NOT NULL,
+    Population int NOT NULL,
+)
+
+SOURCE world-data.sql;
+
+EXIT;

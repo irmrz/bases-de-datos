43f607a62b4587facb83cfcc2592d6f2cfddb9fd
irmrz <ignaciotramirez02@gmail.com>
2023-10-27 18:58:33 -0300

    hasta ej7

--- laboratorio7/practico_7-Ramirez_Ignacio.js
+++ laboratorio7/practico_7-Ramirez_Ignacio.js
@@ -72,3 +72,66 @@ que solo están disponibles en un único idioma y por último tengan un
 rating (“imdb.rating”) mayor a 9 o bien tengan una duración (runtime) de 
 al menos 180 minutos. Listar ordenados por fecha de lanzamiento y número de votos.
 */
+
+db.movies.find(
+    {$and:[
+        {$or: [
+            { genres: "Drama" },
+            { genres: "Action" }
+        ]}, 
+        {languages: {$exists: true}},
+        {languages: {$size: 1}},
+        {$or: [{"imdb.rating":{$gt:9}}, {runtime: {$gte: 180}}]}
+    ]},
+    {title: 1, languages: 1, genres: 1, release: 1, "imdb.votes": 1}
+).sort({release:1, "imdb.votes":-1})
+
+
+/* Ejercicio 6
+Listar el id del teatro (theaterId), estado (“location.address.state”), 
+ciudad (“location.address.city”), y coordenadas (“location.geo.coordinates”)
+de los teatros que se encuentran en algunos de los estados "CA", "NY", 
+"TX" y el nombre de la ciudades comienza con una ‘F’. Listar ordenados 
+por estado y ciudad.
+*/
+
+db.theaters.find(
+    {$and: [
+        {$or: [
+            {"location.address.state": {$eq: "CA"}},
+            {"location.address.state": {$eq: "NY"}},
+            {"location.address.state": {$eq: "TX"}}
+        ]},
+        {"location.address.city": {$regex: /^F/}}]},
+    {theaterId: 1, 
+        "location.address.state": 1, 
+        "location.address.city": 1,
+        "location.geo.coordinates": 1
+    }
+).sort(
+    {"location.address.state": -1, "location.address.city": -1}
+)
+
+
+/* Ejercicio 7
+Actualizar los valores de los campos texto (text) y fecha (date) del 
+comentario cuyo id es ObjectId("5b72236520a3277c015b3b73") a 
+"mi mejor comentario" y fecha actual respectivamente.
+*/
+
+db.comments.updateOne(
+    {_id: {$eq: ObjectId("5b72236520a3277c015b3b73")}},
+    {
+        $set: {text: "mi mejor comentario"},
+        $currentDate: {date: true}
+    }
+)
+
+
+/* Ejercicio 8
+Actualizar el valor de la contraseña del usuario cuyo email es 
+joel.macdonel@fakegmail.com a "some password". La misma 
+consulta debe poder insertar un nuevo usuario en caso que 
+el usuario no exista. Ejecute la consulta dos veces. 
+¿Qué operación se realiza en cada caso?  (Hint: usar upserts).
+*/
